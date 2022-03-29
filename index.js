08784b150b387204fb72368c97098861f4634db1
whitedevil9991 <revanth.pilli@yahoo.com>
2022-03-30 00:38:46 +0530

    main

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonfile = require('jsonfile');
+//we are importing the jsonfile into our index.js
+const moment =require('moment');
+const FILE_PATH = './data.json';
+//grabbing path of data.json file
+
+//
+const DATE = moment().format();
+
+
